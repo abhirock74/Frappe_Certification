696304bf3627fb39d8dd45b21dfb86bf5cc82979
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-08-13 08:19:28 +0530

    add

---
+++ airplane_mode/airplane_mode/doctype/airplane_ticket_add_on_type/airplane_ticket_add_on_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, abhishek kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Airplane Ticket Add-on Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ airplane_mode/airplane_mode/doctype/airplane_ticket_add_on_type/test_airplane_ticket_add_on_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, abhishek kumar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirplaneTicketAddonType(FrappeTestCase):
+	pass
