b723d9a2b1bf78fe5e1e3073d22f8a350b374b0e
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-08-14 12:06:22 +0530

    add aeroplan flight total price

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -7,11 +7,14 @@ from frappe.model.document import Document
 
 class AirplaneTicket(Document):
 	def before_save(self):
-		Add_ons = self.Add_ons
-		total_amount = self.flight_price
+		if self.status != 'Boarded':
+			Add_ons = self.Add_ons
+			total_amount = self.flight_price
 
-		for item in Add_ons:
-			total_amount += item.amount
+			for item in Add_ons:
+				total_amount += item.amount
 			
-		self.total_amount = total_amount
+			self.total_amount = total_amount
+		else:
+			return
 			
