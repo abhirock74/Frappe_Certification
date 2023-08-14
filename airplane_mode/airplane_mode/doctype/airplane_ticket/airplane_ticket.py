b29535ed8a25615ebe8b90e151fc04c1b076a95c
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-08-14 10:50:36 +0530

    Add auto add addon price to total flight amount

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -7,4 +7,11 @@ from frappe.model.document import Document
 
 class AirplaneTicket(Document):
 	def before_save(self):
-		print(self)
+		Add_ons = self.Add_ons
+		total_amount = self.flight_price
+
+		for item in Add_ons:
+			total_amount += item.amount
+			
+		self.total_amount = total_amount
+			
