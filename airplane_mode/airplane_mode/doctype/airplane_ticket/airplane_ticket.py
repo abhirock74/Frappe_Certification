8217cef4b34dcf35d880f7097fc8bd44a4428806
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-08-14 12:43:58 +0530

    add auto seat aleartment

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -2,19 +2,26 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
 
+from frappe.model.document import Document
+import random
+import string
 
 class AirplaneTicket(Document):
-	def before_save(self):
-		if self.status != 'Boarded':
-			Add_ons = self.add_ons
-			total_amount = self.flight_price
 
-			for item in Add_ons:
-				total_amount += item.amount
-			
-			self.total_amount = total_amount
-		else:
-			return
-			
+  def before_save(self):
+        if self.status != 'Boarded':
+            Add_ons = self.add_ons
+            total_amount = self.flight_price
+
+            for item in Add_ons:
+                total_amount += item.amount
+
+            self.total_amount = total_amount
+
+            random_integer = random.randint(1, 100)
+            random_letter = random.choice(string.ascii_uppercase[:5])
+            seat = f"{random_integer}{random_letter}"  # Using f-string for formatting
+            self.seat = seat
+        else:
+            return
