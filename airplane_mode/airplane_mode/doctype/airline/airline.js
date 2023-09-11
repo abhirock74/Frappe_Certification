90d963b875747889775ee7a2125298f55314e846
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-09-11 18:16:52 +0530

    add website url

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2023, abhishek kumar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airline", {
+	refresh(frm) {  
+        if(frm.doc.website){
+            frm.add_web_link(frm.doc.website)
+        }
+	},
+    website: function(frm){
+        frm.add_web_link(frm.doc.website)
+    }
 
-// 	},
-// });
+});
