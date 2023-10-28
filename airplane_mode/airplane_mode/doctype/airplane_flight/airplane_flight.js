17f492af71928b693ff26c4556ab18e9796fca49
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-28 13:42:04 +0530

    add hide advance filter to dropdown

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
@@ -3,7 +3,14 @@
 
 frappe.ui.form.on("Airplane Flight", {
 	refresh(frm) {
-        console.log("new")
+    // hide advance search and create new option in lists
+    frm.set_df_property('airplane', 'only_select', true);
+    frm.set_df_property('source_airport', 'only_select', true);
+    frm.set_df_property('destination_airport', 'only_select', true);
+    frm.set_df_property('pilot', 'only_select', true);
+    frm.set_df_property('flight_attendants', 'only_select', true);
+    frm.set_df_property('co_pilot', 'only_select', true);
+    // //////////////////////////////////////////////////////////////////
         frm.fields_dict["destination_airport"].get_query = function (doc) {
             return {
               filters: {
