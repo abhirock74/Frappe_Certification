7340aec1285cbfb335821b0f331b4a115c93a2cb
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-11-05 07:46:51 +0530

    add custom reports form availability of shop or not

---
+++ airplane_mode/airport_shop_management/report/shops_are_available_or_booked/shops_are_available_or_booked.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, abhishek kumar and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["shops are available or booked"] = {
+	"filters": [
+
+	]
+};
