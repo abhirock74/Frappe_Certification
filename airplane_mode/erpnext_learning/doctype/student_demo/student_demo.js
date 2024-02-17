c3b4231d2ae27a8eb63b0a8fc23cb9a33944fabc
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-02-17 17:01:14 +0530

    add changes

---
+++ airplane_mode/erpnext_learning/doctype/student_demo/student_demo.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, abhishek kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student Demo", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ airplane_mode/erpnext_learning/doctype/student_demo/test_student_demo.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, abhishek kumar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStudentDemo(FrappeTestCase):
+	pass
